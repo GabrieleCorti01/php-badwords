f1522f12cfde0b30cadeb557da3bd08e1cfbeee0
Gabrielecorti01 <gcorti.info@gmail.com>
2021-10-12 15:50:11 +0200

    Esercizio completato, non riesco a mandare a capo il counter

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     
     $text = "In una terra lontana, dietro le montagne Parole, lontani dalle terre di Vocalia e Consonantia, vivono i testi casuali. Vivono isolati nella cittadina di Lettere, sulle coste del Semantico, un immenso oceano linguistico. Un piccolo ruscello chiamato Devoto Oli attraversa quei luoghi, rifornendoli di tutte le regolalie di cui hanno bisogno. ";
     $badwords = $_GET["censore"];
+    $text = str_replace($badwords,"***",$text);
 ?>
 
 <!DOCTYPE html>
@@ -23,9 +24,7 @@
     <?php echo $text . strlen($text) ?>
 </p>
 
-<p>
-    <?php echo $badwords ?>
-</p>
+
     
 </body>
 </html>
