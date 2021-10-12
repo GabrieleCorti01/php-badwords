4f2d1499ce95bceb29da210055f54e45a66779d2
Gabrielecorti01 <gcorti.info@gmail.com>
2021-10-12 15:40:58 +0200

    Non riesco ad indicare la parola da censurare

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<?php
+    
+    $text = "In una terra lontana, dietro le montagne Parole, lontani dalle terre di Vocalia e Consonantia, vivono i testi casuali. Vivono isolati nella cittadina di Lettere, sulle coste del Semantico, un immenso oceano linguistico. Un piccolo ruscello chiamato Devoto Oli attraversa quei luoghi, rifornendoli di tutte le regolalie di cui hanno bisogno. ";
+    $badwords = $_GET["censore"];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esercizo php!ß</title>
+</head>
+<body>
+
+<h1>Inserisci: censore= "parola che vuoi censurare" all'interno dell'url per censurare una parola a tua scelta</h1>
+
+
+
+<p>
+    <?php echo $text . strlen($text) ?>
+</p>
+
+<p>
+    <?php echo $badwords ?>
+</p>
+    
+</body>
+</html>
